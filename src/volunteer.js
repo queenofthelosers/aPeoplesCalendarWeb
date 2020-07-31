978f14aed350db7f7683aa3beeff34a82288418b
Trevor T. Smith <tts2p4@gmail.com>
2020-07-31 08:07:40 -0500

    Major updates to font-sizing below 500px on all three views.

--- src/volunteer.js
+++ src/volunteer.js
@@ -6,7 +6,7 @@ export function Volunteer() {
     <div id='volunteerWrapper'>
       <div id='volunteerContent'>
         <header id='volunteerHeader'>
-          <p id='headerText'>Volunteer</p>
+          <p id='volunteerHeaderText'>Volunteer</p>
         </header>
         <p className='volunteerText'>Whether you are an expert in African labor history, a developer with feature ideas, or are just someone who can proofread calendar events, we welcome your efforts and suggestions towards helping make our calendar more comprehensive and easier to use.</p>
         <ul>
@@ -17,7 +17,7 @@ export function Volunteer() {
             <p className='volunteerListText'>Are you a developer who has a feature idea? <a className='volunteerLink' target='_blank' rel="noopener noreferrer" href='https://github.com/huntzinger92/aPeoplesCalendarWeb'>Our code is open-source</a>.</p>
           </li>
           <li>
-            <p className='volunteerListText'>Have a different way to help? Feel free to contact us at <a className='volunteerLink' target='_blank' rel="noopener noreferrer" id='emailLink' href='mailto:apeoplescalendar@gmail.com'>apeoplescalendar@gmail.com</a>.
+            <p className='volunteerListText'>Have a different way to help? Feel free to contact us via <a className='volunteerLink' target='_blank' rel="noopener noreferrer" id='emailLink' href='mailto:apeoplescalendar@gmail.com'>email</a>.
           </p>
           </li>
         </ul>
